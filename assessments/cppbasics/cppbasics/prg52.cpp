5baa9ec5235f2e9281f5b0780a1e96ba6596f6ed
SreelakshmiS123 <lakshmi644sree@gmail.com>
2025-05-12 08:47:07 +0530

    updated

---
+++ assessments/cppbasics/cppbasics/prg52.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+#include <limits>
+using namespace std;
+int main() {
+	int num;
+	cout << "enter a number: ";
+	cin >> num;
+	if (num<INT_MIN || num>INT_MAX)
+	{
+		cout << "The number is out of integer limits" << endl;
+	}
+	else
+	{
+		cout << "The number is within integer limit" << endl;
+	}
+
+	return 0;
+}
